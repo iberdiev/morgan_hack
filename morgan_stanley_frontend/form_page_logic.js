33d07a6d50f7fce31dcaf2f6d6c8ed415c739759
Abdumalik Abidzhanov <abidzhanov_a@outlook.com>
2022-10-03 23:01:11 +0200

    Test3 page + form page logic implemented and one more table added

---
+++ morgan_stanley_frontend/form_page_logic.js
@@ -0,0 +1,43 @@
+class FormPage {
+    constructor() {
+        this.userData = {};
+        this.formElement = document.querySelector('.user_form');
+        this.formValues = document.querySelectorAll('.form-value');
+        this.formElement.addEventListener('submit', this.onFormSubmit.bind(this));
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+        this.getUserData();
+        this.packUserData();
+        console.log(this.userData);
+    }
+
+    getUserData() {
+        this.formValues.forEach(e => this.userData[e.id] = e.value);
+    }
+
+    packUserData() {
+        const testData = JSON.parse(localStorage.getItem('data'));
+        const timeFinished = JSON.parse(localStorage.getItem('time_finish'));
+
+        this.userData['testData'] = testData;
+        this.userData['timeFinished'] = timeFinished;
+    }
+
+    #sendUserData() {
+        
+        postData('https://example.com/answer', )
+        .then((response) => response.blob())
+        .then((data) => {
+            console.log(data); // JSON data parsed by `data.json()` call
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+    }
+}
+
+const formPage = new FormPage();
+
